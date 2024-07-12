3b01dfe50ac17ebd1d83530ab9a86b853e74f31b
Jon Mico <jonmico@gmail.com>
2024-07-12 15:29:25 -0400

    Add Process function to prices.go.

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"example.com/price-calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
+
 	taxRates := []float64{0, 0.07, 0.10, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (taxRate + 1)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,9 +1,21 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (job.TaxRate + 1)
+	}
+
+	fmt.Println(result)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
